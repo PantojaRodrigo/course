1d7ef465da2da954ba8107e2610693217ea21ac1
PantojaRodrigo <rodrigopanv@outlook.com>
2023-09-12 00:53:27 -0600

    tarea TDD OO Bowling game

--- inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.spec.ts
@@ -8,4 +8,66 @@ describe('Test Bowling Game Functionality', () => {
     game = new BowlingGame()
   });
 
+  it('should create game', () => { 
+    expect(game).toBeDefined();
+  })
+
+  it('can throw a ball', () => {
+    
+    expect(game.roll(2)).toBe(undefined);
+  });
+
+  it('should calculate score with all zero', () => {
+    game = rollMany(20, 0);
+
+    expect(game.score()).toBe(0)
+  });
+  it('should calculate score with all one', () => {
+    game = rollMany(20, 1);
+
+    expect(game.score()).toBe(20)
+  });
+
+  it('can roll a spare ', () => {
+    rollSpare(game)
+    game.roll(3)
+
+    rollMany( 17, 0);
+
+    expect(game.score()).toBe(16)
+  });
+
+  it('can roll a strike', () => {
+    rollStrike(game);
+    game.roll(2);
+    game.roll(2);  
+
+    rollMany( 16, 0);
+
+    expect(game.score()).toBe(18)
+  });
+
+  it('roll a perfect game', () => {
+    game = rollMany( 12, 10);
+
+    expect(game.score()).toBe(300)
+  });
+
+
+  function rollStrike(game) {
+    game.roll(10)
+  }
+  
+  function rollSpare(game) {
+    game.roll(5);
+    game.roll(5);
+  }
+  
+  function rollMany(rolls: number, pins: number) {
+    for (let i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  
+    return game;
+  }
 });
--- inclass/src/3_TDD/objectOriented/BowlingGame.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.ts
@@ -1,6 +1,58 @@
 
 export class BowlingGame {
 
-  constructor() { }
+  private rolls: number[];
+  
+  constructor() {
+    this.rolls = [];
+  }
+  
+  public roll(pins: number): void {
+    this.rolls.push(pins);
+  }
 
+  public score(): number {
+    const FRAMES = 10
+    let score = 0
+    let indexRoll = 0
+    let rolls=this.rolls;
+    for (let i = 0; i < FRAMES; i++) {
+
+      if (this.isStrike(indexRoll)) {
+        score += this.scoreForStrike(indexRoll)
+        indexRoll += 1
+  
+      } else if (this.isSpare(indexRoll)) {
+        score += this.scoreForSpare(indexRoll)
+        indexRoll += 2
+  
+      } else {
+        score += this.scoreForFrame(indexRoll)
+        indexRoll += 2
+  
+      }
+  
+    }
+    return score;
+  }
+
+  private scoreForStrike( index: number) {
+    return 10 + this.rolls[index + 1] + this.rolls[index + 2]
+  }
+  
+  private isStrike(index: number) {
+    return this.rolls[index] === 10
+  }
+  
+  private scoreForFrame(index: number) {
+    return this.rolls[index] + this.rolls[index + 1]
+  }
+  
+  private scoreForSpare(index: number) {
+    return 10 + this.rolls[index + 2]
+  }
+  
+  private isSpare(index: number) {
+    return this.rolls[index] + this.rolls[index + 1] === 10
+  }
 }
